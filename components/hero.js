ecc0471fdc734c04b1320e93aa55f9ccf7d96ee9
WANG <e23003@std.it-college.ac.jp>
2023-08-10 20:51:03 +0900

    3.5 hero module css finished

--- components/hero.js
+++ components/hero.js
@@ -1,7 +1,11 @@
+import style from 'styles/hero.module.css'
+
 const Hero = ({ title, subtitle, imageOn = false }) => (
   <div>
-    <h1>{title}</h1>
-    <p>{subtitle}</p>
+    <div className={style.text}>
+      <h1 className={style.title}>{title}</h1>
+      <p className={style.subtitle}>{subtitle}</p>
+    </div>
     {imageOn && <figure> [画像] </figure>}
   </div>
 )
