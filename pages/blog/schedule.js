a2027e92d8ed7d543ec78f8dafee7aa32da0d497
WANG <e23003@std.it-college.ac.jp>
2023-10-19 19:29:36 +0900

    chapter7 data get

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,21 +1,49 @@
 import { client } from 'lib/api'
+//import { getPostBySlug } from 'lib/api'
+import Container from 'components/container'
 
-const Schedule = () => {
-  return <div></div>
+//{ title, publish, content, eyecatch, categories }
+const Schedule = ({ title }) => {
+  return (
+    <Container>
+      <h1>{title}</h1>
+    </Container>
+  )
 }
-
-const getStaticProps = () => {
+const getStaticProps = async () => {
   const resPromise = client.get({
-    endpoint: 'blogs'
+    endpoint: 'blogs',
+    queries: { filters: 'slug[equals]schedule' }
   })
-  resPromise
-    .then(res => res.contents)
-    .then(contents => contents.at(0))
-    .then(content => console.log(content))
+  const res = await resPromise
+  const contents = res.contents.at(0)
+  //console.log(contents)
   return {
-    props: {}
+    props: { title: contents.title }
   }
+  // return {
+  //   props: {
+  //     title: post.title,
+  //     publish: post.publishDate,
+  //     content: post.content,
+  //     eyecatch: post.eyecatch,
+  //     categories: post.categories
+  //   }
 }
 
+// const getStaticProps = () => {
+
+//   const resPromise = client.get({
+//     endpoint: 'blogs'
+//   })
+//   resPromise
+//     .then(res => res.contents)
+//     .then(contents => contents.at(0))
+//     .then(content => console.log(content))
+//   return {
+//     props: {}
+//   }
+// }
 export { getStaticProps }
+
 export default Schedule
